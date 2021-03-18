220dc60140121e2c5274b6d9cddf1f9aa351e299
DragonWen <18475536452@163.com>
2021-03-18 22:47:23 +0800

    Initial commit

---
+++ LearnApachePoi/src/main/java/com/dragon/learnapachepoi/LearnApachePoiApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.learnapachepoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnApachePoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnApachePoiApplication.class, args);
+    }
+
+}
---
+++ LearnApachePoi/src/test/java/com/dragon/learnapachepoi/LearnApachePoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.learnapachepoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApachePoiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
